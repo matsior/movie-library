54153db2b03e72d032040a097f2ca780d994db48
Mateusz Krajewski <maatsior@gmail.com>
2022-08-06 15:23:17 +0200

    Initial commit

---
+++ src/main/java/com/matsior/movielibrary/MovieController.java
@@ -0,0 +1,13 @@
+package com.matsior.movielibrary;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MovieController {
+
+    @GetMapping("/test")
+    public int test() {
+        return 1;
+    }
+}
